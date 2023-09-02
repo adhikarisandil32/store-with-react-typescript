44f91afbb6179413394e4145b6b7cabe953bb9ce
Sandil Adhikari <adhikarisandil32@gmail.com>
2023-09-03 01:05:12 +0545

    go to suggestion from homepage and aboutpage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom"
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <h1>
+      Go to <Link to="/store">Store</Link>
+    </h1>
+  )
 }
